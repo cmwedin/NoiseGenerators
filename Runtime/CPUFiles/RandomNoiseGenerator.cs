d5c6261598c1c4c9363db2c6f04ac5ead018c174
cmwedin <cmwedin@gmail.com>
2022-09-29 16:43:09 -0400

    RandomNoise display
    added a quad on which the random noise tex is displayed and verified its functionality

--- Runtime/CPUFiles/RandomNoiseGenerator.cs
+++ Runtime/CPUFiles/RandomNoiseGenerator.cs
@@ -8,33 +8,43 @@ namespace SadSapphicGames.NoiseGenerators
     {
         public ComputeShader randomNoiseShader;
         public RenderTexture noiseTexture;
+        public MeshRenderer displayMeshRenderer;
         public uint seed;
         public uint texWidth;
         public uint texHeight;
         // Start is called before the first frame update
         void Start()
         {
-            noiseTexture = new RenderTexture((int)texWidth, (int)texHeight, 24);
-            noiseTexture.enableRandomWrite = true;
-            noiseTexture.Create();
-
-            randomNoiseShader.SetTexture(0, "_NoiseTexture", noiseTexture);
-            randomNoiseShader.SetInt("_Seed", (int)seed);
-            randomNoiseShader.SetInt("_TexWidth", (int)texWidth);
-            randomNoiseShader.SetInt("_TexHeight", (int)texHeight);
-
-            GenerateTexture();
+            // GenerateTexture();
         }
 
         // Update is called once per frame
-        void Update()
-        {
-
+        void Update() {
         }
 
-        public void GenerateTexture()
-        {
+        private void SetShaderParameters() {
+            randomNoiseShader.SetInt("_Seed", (int)seed);
+            randomNoiseShader.SetInt("_TexWidth", (int)texWidth);
+            randomNoiseShader.SetInt("_TexHeight", (int)texHeight);
+            randomNoiseShader.SetTexture(0, "_NoiseTexture", noiseTexture);
+        }
+
+        public void GenerateTexture() {
+            noiseTexture = new RenderTexture((int)texWidth, (int)texHeight, 24);
+            noiseTexture.enableRandomWrite = true;
+            noiseTexture.Create();
+            SetShaderParameters();
             randomNoiseShader.Dispatch(0, noiseTexture.width / 8, noiseTexture.height / 8, 1);
+            displayMeshRenderer.material.mainTexture = noiseTexture;
         }
+        public void DisplayTexture() {
+        }
+        private void OnPreRender() {
+            Camera.main.targetTexture = noiseTexture;
+        }
+        private void OnPostRender() {
+            Graphics.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), noiseTexture);
+            Camera.main.targetTexture = null;
+        }        
     }
 }
