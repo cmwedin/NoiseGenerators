857d82b1d48f0b0f5160b0ab1f27417210ca614f
cmwedin <cmwedin@gmail.com>
2022-10-20 12:31:31 -0400

    PerlinNoiseGenerator xml comments

--- Runtime/Components/PerlinNoiseGeneratorComponent.cs
+++ Runtime/Components/PerlinNoiseGeneratorComponent.cs
@@ -12,43 +12,26 @@ namespace SadSapphicGames.NoiseGenerators
                 CreateGeneratorObject();
             }
             return noiseGeneratorObject;
-            
         }}
 
-        [SerializeField] bool tileTexture;
+        /// <summary>
+        /// If the texture should be required to tile seamlessly
+        /// </summary>
+        [SerializeField,Tooltip("If the texture should be required to tile seamlessly")] bool tileTexture;
+        /// <summary>
+        /// The pixel size of a single lattice cell
+        /// </summary>
+        [SerializeField,Tooltip("The pixel size of a single lattice cell")] private Vector2Int latticeCellSize;
 
-        [SerializeField] private Vector2Int latticeSize;
-
-
-        // public Vector2Int LatticeSize {
-        //     get {
-        //         if(TexWidth % latticeSize.x != 0) {
-        //             Debug.LogWarning("Horizontal lattice size not set to factor of texture width, adjusting value to closest factor");
-        //             latticeSize = new Vector2Int(
-        //                 HelperMethods.FindClosestFactor((int)TexWidth,latticeSize.x),
-        //                 latticeSize.y
-        //             );
-        //         } if (TexHeight % latticeSize.y != 0) {
-        //             Debug.LogWarning("Vertical lattice size not set to factor of texture height, adjusting value to closest factor");
-        //             latticeSize = new Vector2Int(
-        //                 latticeSize.x,
-        //                 HelperMethods.FindClosestFactor((int)TexHeight,latticeSize.y)
-        //             );
-        //         }
-        //         return latticeSize; 
-        //     }
-        // }
-
-        // private int latticeTexWidth { get => Mathf.CeilToInt((float)texWidth / (float)LatticeSize.x)+1; }
-        // private int latticeTexHeight { get => Mathf.CeilToInt((float)texHeight / (float)LatticeSize.y)+1; }
-
-        // Start is called before the first frame update
+        /// <summary>
+        /// Constructs a PerlinNoiseGenerator and sets it's RequireSeamlessTiling property
+        /// </summary>
         protected override void CreateGeneratorObject()
         {
             if(noiseGeneratorObject != null) {
                 noiseGeneratorObject.Dispose();
             }
-            noiseGeneratorObject = new PerlinNoiseGenerator(TexWidth, TexHeight, seed, latticeSize);
+            noiseGeneratorObject = new PerlinNoiseGenerator(TexWidth, TexHeight, seed, latticeCellSize);
             NoiseGeneratorObject.RequireSeamlessTiling = tileTexture;
             disposedValue = false;
         }
@@ -58,23 +41,19 @@ namespace SadSapphicGames.NoiseGenerators
         //     noiseGeneratorObject.LatticeCellSize = latticeSize;
         //     NoiseGeneratorObject.RequireSeamlessTiling = tileTexture;
         // }
-        protected override void OnDisable()
-        {
-            base.OnDisable();
-        }
-        protected override void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                Debug.Log("Disposing noise generator");
-                if (disposing)
-                {
-                    // TODO: dispose managed state (managed objects)
-                }
+        // protected override void Dispose(bool disposing)
+        // {
+        //     if (!disposedValue)
+        //     {
+        //         Debug.Log("Disposing noise generator");
+        //         if (disposing)
+        //         {
+        //             // TODO: dispose managed state (managed objects)
+        //         }
 
-                noiseGeneratorObject.Dispose();
-                disposedValue = true;
-            }
-        }
+        //         noiseGeneratorObject.Dispose();
+        //         disposedValue = true;
+        //     }
+        // }
     }
 }
