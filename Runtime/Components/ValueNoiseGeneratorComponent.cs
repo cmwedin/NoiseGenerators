aea42194b4a26c2708d654edfaf1fd8c1826c762
cmwedin <cmwedin@gmail.com>
2022-10-20 12:31:57 -0400

    ValueNoiseGenComponent xml comments

--- Runtime/Components/ValueNoiseGeneratorComponent.cs
+++ Runtime/Components/ValueNoiseGeneratorComponent.cs
@@ -12,32 +12,14 @@ namespace SadSapphicGames.NoiseGenerators
             }
             return noiseGeneratorObject;
         }}
-
-        [SerializeField] private bool tileTexture;
-
-        [SerializeField] private Vector2Int latticeCellSize;
-        // public Vector2Int LatticeSize {
-        //     get {
-        //         if(TexWidth % latticeSize.x != 0) {
-        //             Debug.LogWarning("Horizontal lattice size not set to factor of texture width, adjusting value to closest factor");
-        //             latticeSize = new Vector2Int(
-        //                 HelperMethods.FindClosestFactor((int)TexWidth,latticeSize.x),
-        //                 latticeSize.y
-        //             );
-        //         } if (TexHeight % latticeSize.y != 0) {
-        //             Debug.LogWarning("Vertical lattice size not set to factor of texture height, adjusting value to closest factor");
-        //             latticeSize = new Vector2Int(
-        //                 latticeSize.x,
-        //                 HelperMethods.FindClosestFactor((int)TexHeight,latticeSize.y)
-        //             );
-        //         }
-        //         return latticeSize; 
-        //     }
-        // }
-
-
-        // private int latticeTexWidth { get => Mathf.CeilToInt((float)texWidth / (float)LatticeSize.x)+1; }
-        // private int latticeTexHeight { get => Mathf.CeilToInt((float)texHeight / (float)LatticeSize.y)+1; }
+        /// <summary>
+        /// If the texture should be required to tile seamlessly
+        /// </summary>
+        [SerializeField,Tooltip("If the texture should be required to tile seamlessly")] private bool tileTexture;
+        /// <summary>
+        /// The pixel size of a single lattice cell
+        /// </summary>
+        [SerializeField,Tooltip("The pixel size of a single lattice cell")] private Vector2Int latticeCellSize;
 
         protected override void CreateGeneratorObject() {
             if(noiseGeneratorObject != null) {
@@ -47,10 +29,10 @@ namespace SadSapphicGames.NoiseGenerators
             NoiseGeneratorObject.RequireSeamlessTiling = tileTexture;
             disposedValue = false;
         }
-        protected override void OnDisable()
-        {
-            base.OnDisable();
-        }
+        // protected override void OnDisable()
+        // {
+        //     base.OnDisable();
+        // }
         // protected override void OnValidate()
         // {
         //     base.OnValidate();
