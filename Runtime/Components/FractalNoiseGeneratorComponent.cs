b4d7896a59982b4995b1513f806b2b0b0ddcdfde
cmwedin <cmwedin@gmail.com>
2022-10-27 13:29:16 -0400

    FractalGeneratorComponent properties
    adds properties for modifiying the fields of a fractal generator component from code

--- Runtime/Components/FractalNoiseGeneratorComponent.cs
+++ Runtime/Components/FractalNoiseGeneratorComponent.cs
@@ -20,28 +20,40 @@ namespace SadSapphicGames.NoiseGenerators
         /// <summary>
         /// The number of times detail should be added onto the noise texture
         /// </summary>
+        public uint Octaves { get => octaves; set => octaves = value; }
+
         [SerializeField,Tooltip("The number of times detail should be added onto the noise texture")] private uint octaves;
         /// <summary>
         /// The factor by which the frequency should increase with each octave
         /// </summary>
+        public float Lacunarity { get => lacunarity; set => lacunarity = value; }
+
         [SerializeField,Tooltip("The factor by which the frequency should increase with each octave")] private float lacunarity = 2;
         /// <summary>
         /// The initial frequency in the first octave
         /// </summary>
+        public float Frequency { get => frequency; set => frequency = value; }
+
         [SerializeField,Tooltip("The initial frequency in the first octave")] private float frequency = 2;
 
         /// <summary>
         /// The factor by which the amplitude should decrease each octave
         /// </summary>
+        public float Gain { get => gain; set => gain = value; }
+
         [SerializeField,Tooltip("The factor by which the amplitude should decrease each octave")] private float gain = .5f;
         /// <summary>
         /// The initial amplitude in the first octaves, changing this has no affect is normalizeAmplitude is true
         /// </summary>
+        public float Amplitude { get => amplitude; set => amplitude = value; }
+
         [SerializeField] private float amplitude = .5f;
         /// <summary>
         /// If the affect of the initial amplitude should be normalized out of the final value
         /// </summary>
+        public bool NormalizeAmplitude { get => normalizeAmplitude; set => normalizeAmplitude = value; }
         [SerializeField,Tooltip("If the affect of the initial amplitude should be normalized out of the final value")] private bool normalizeAmplitude = true;
+
 
         protected override AbstractNoiseGenerator CreateGeneratorObject()
         {
@@ -49,8 +61,8 @@ namespace SadSapphicGames.NoiseGenerators
             baseNoiseGenerator.TexWidth = TexWidth;
             baseNoiseGenerator.Seed = seed;
             baseNoiseGenerator.GenerateTexture();
-            var noiseGeneratorObject = new FractalNoiseGenerator(octaves, baseNoiseGenerator.NoiseTexture, lacunarity, frequency, gain, amplitude);
-            noiseGeneratorObject.NormalizeAmplitude = normalizeAmplitude;
+            var noiseGeneratorObject = new FractalNoiseGenerator(Octaves, baseNoiseGenerator.NoiseTexture, Lacunarity, Frequency, Gain, Amplitude);
+            noiseGeneratorObject.NormalizeAmplitude = NormalizeAmplitude;
             return noiseGeneratorObject;
         }
     }
