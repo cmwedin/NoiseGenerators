e5fc18154457be2ef8e22090eab787bffcd0e8c0
cmwedin <cmwedin@gmail.com>
2022-11-05 12:39:32 -0400

    generator editor texture preview
    adds a preview of the generated texture to the inspector of generator components

--- Editor/CustomInspectors/NoiseGenInspector.cs
+++ Editor/CustomInspectors/NoiseGenInspector.cs
@@ -10,10 +10,27 @@ namespace SadSapphicGames.NoiseGeneratorsEditor
     public class NoiseGenInspector : Editor
     {
         AbstractNoiseGeneratorComponent targetGenerator { get => (AbstractNoiseGeneratorComponent)target; }
+        int previewSize = 100;
+
         public override void OnInspectorGUI()
         {
             if(GUILayout.Button("Generate Texture")){
                 targetGenerator.GenerateTexture();
+            }
+            if (targetGenerator.TextureGenerated)
+            {
+                EditorGUILayout.BeginVertical();
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.Label("Texture Preview Size:");
+                previewSize = EditorGUILayout.IntSlider(previewSize,0,Mathf.Min(targetGenerator.NoiseTexture.width,Screen.width-20));
+                EditorGUILayout.EndHorizontal();
+                using (var l = new EditorGUILayout.HorizontalScope())
+                {
+                    GUILayout.FlexibleSpace();
+                    GUILayout.Label(targetGenerator.NoiseTexture, new GUILayoutOption[2] { GUILayout.Width(previewSize), GUILayout.Height(previewSize) });
+                    GUILayout.FlexibleSpace();
+                }
+                EditorGUILayout.EndVertical();
             }
             base.OnInspectorGUI();
         }
